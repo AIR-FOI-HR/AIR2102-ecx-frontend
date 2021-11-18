bf266318f21f86032ed054ebcc63e9db36cd659b
Seljumnik <bixi36@gmail.com>
2021-11-18 18:06:59 +0100

    Project prepared for work. Fixed terrible mess that awaited me from earlier commit on develop. Added simple splash screen with Wienerberger logo.

---
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.ecxfoi.wbl.wienerbergerfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
