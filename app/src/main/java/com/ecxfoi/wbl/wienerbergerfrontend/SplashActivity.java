72231f804f22898abce855d25d3959b6b6bb573e
Seljumnik <bixi36@gmail.com>
2021-11-18 18:13:56 +0100

    Title bar hidden. Splash screen is now without title bar.

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/SplashActivity.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/SplashActivity.java
@@ -3,12 +3,24 @@ package com.ecxfoi.wbl.wienerbergerfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // will hide the title
+
+        if (getSupportActionBar() != null) {
+        getSupportActionBar().hide(); // hide the title bar
+        }
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // enable full screen
+
+        setContentView(R.layout.activity_splash); // show splash screen with Wienerberger logo
     }
 }
