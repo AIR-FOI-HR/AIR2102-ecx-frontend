75759c9e2cc66e9fbcc4fe11d29a043bcf95e411
Seljumnik <bixi36@gmail.com>
2021-11-25 00:11:25 +0100

    WBL-15 Retrofit and APIService Implemented service for accessing Login route via Retrofit and POST method. Fixed permissions for internet access.

---
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/api/APIRoutes.java
@@ -0,0 +1,7 @@
+package com.ecxfoi.wbl.wienerbergerfrontend.api;
+
+public class APIRoutes
+{
+    public final static String BASE_URL = "http://192.168.5.17:8080";
+    public final static String ROUTE_LOGIN = "/api/login";
+}
