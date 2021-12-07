18ec442cdf82acfd309924f1d780c7632d757a57
Lovro <lovro.posaric@gmail.com>
2021-12-07 16:56:47 +0100

    WBL-14 Added my account interface (getting & updating data implemented)

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/api/APIService.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/api/APIService.java
@@ -2,14 +2,23 @@ package com.ecxfoi.wbl.wienerbergerfrontend.api;
 
 import com.ecxfoi.wbl.wienerbergerfrontend.auth.AuthenticationData;
 import com.ecxfoi.wbl.wienerbergerfrontend.auth.AuthenticationRequest;
+import com.ecxfoi.wbl.wienerbergerfrontend.models.UserData;
 import com.ecxfoi.wbl.wienerbergerfrontend.models.WienerbergerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface APIService
 {
     @POST(APIRoutes.BASE_URL + APIRoutes.ROUTE_LOGIN)
     Call<WienerbergerResponse<AuthenticationData>> createUser(@Body AuthenticationRequest user);
+
+    @GET(APIRoutes.BASE_URL + APIRoutes.ROUTE_USERS)
+    Call<WienerbergerResponse<UserData>> getCurrentUserData();
+
+    @PUT(APIRoutes.BASE_URL + APIRoutes.ROUTE_USERS)
+    Call<WienerbergerResponse<UserData>> updateCurrentUserData(@Body UserData userData);
 }
--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/myaccount/MyAccountFragment.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/myaccount/MyAccountFragment.java
@@ -1,9 +1,22 @@
 package com.ecxfoi.wbl.wienerbergerfrontend.ui.main.myaccount;
 
-import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.ecxfoi.wbl.wienerbergerfrontend.R;
 import com.ecxfoi.wbl.wienerbergerfrontend.base.BaseFragment;
-import com.ecxfoi.wbl.wienerbergerfrontend.ui.main.home.HomeViewModel;
+import com.ecxfoi.wbl.wienerbergerfrontend.databinding.ActivityMainBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.databinding.FragmentMyAccountBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.models.UserData;
 
 import javax.inject.Inject;
 
@@ -13,10 +26,33 @@ public class MyAccountFragment extends BaseFragment<MyAccountViewModel>
     ViewModelProvider.Factory factory;
 
     private MyAccountViewModel viewModel;
+    private FragmentMyAccountBinding binding;
 
     @Override
-    public MyAccountViewModel getViewModel() {
+    public MyAccountViewModel getViewModel()
+    {
         viewModel = new ViewModelProvider(this, factory).get(MyAccountViewModel.class);
         return viewModel;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        binding = FragmentMyAccountBinding.inflate(inflater, container, false);
+        binding.setMyAccountViewModel(viewModel);
+
+        binding.btnUpdate.setOnClickListener(v -> {
+            viewModel.updateUserData();
+        });
+        binding.btnCancel.setOnClickListener(v -> Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.action_myAccountFragment_to_homeFragment));
+
+        viewModel.getCurrentUserData().observe(getViewLifecycleOwner(), this::setUserData);
+
+        return binding.getRoot();
+    }
+
+    private void setUserData(UserData userData)
+    {
+        viewModel.setUserData(userData);
     }
 }
