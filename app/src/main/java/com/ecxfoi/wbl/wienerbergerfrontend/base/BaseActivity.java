fe3cb82573bc8226d89170d47c1d7cc79256fe3d
Mislav Matijević <mislav.mim@gmail.com>
2021-12-22 19:41:29 +0100

    WBL-55 "Back" triggers Alert Dialog Now each Activity automatically displays a yes/no dialog when back is pressed. The dialog asks whether to close the app. If yes is selected, application exits completely.

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/base/BaseActivity.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/base/BaseActivity.java
@@ -3,18 +3,34 @@ package com.ecxfoi.wbl.wienerbergerfrontend.base;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.ViewModel;
+
+import com.ecxfoi.wbl.wienerbergerfrontend.utils.ExitActivity;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 public abstract class BaseActivity<T extends ViewModel> extends DaggerAppCompatActivity
 {
     private T viewModel;
+
     public abstract T getViewModel();
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         this.viewModel = viewModel == null ? getViewModel() : viewModel;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit?")
+                .setMessage("Do you really want to close Wienerberger Application?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> ExitActivity.exitApplication(this))
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
