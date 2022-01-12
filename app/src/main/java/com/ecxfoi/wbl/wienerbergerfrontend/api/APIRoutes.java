5f9716c739ec33dcdffda89051ffb91c2176d449
Lovro <lovro.posaric@gmail.com>
2022-01-12 13:09:39 +0100

    [WBL-57] Added a material balance interface

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/api/APIRoutes.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/api/APIRoutes.java
@@ -8,7 +8,8 @@ public class APIRoutes
     public final static String ROUTE_COMPANIES = "/api/companies";
     public final static String ROUTE_ORDERS = "/api/orders";
     public final static String ROUTE_ORDER_DETAILS = "/api/orders/details";
-    public final static String ROUTE_CUSTOMERS = "/api/customers";
+    public final static String ROUTE_CUSTOMERS = "/api/companies";
+    public final static String ROUTE_CUSTOMER_MATERIALS = "/api/companies/materials";
     public final static String ROUTE_DELIVERY_NOTES = "/api/delivery_notes";
     public static final String ROUTE_TICKETS = "/api/tickets";
 }
--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/materialbalance/MaterialBalanceFragment.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/materialbalance/MaterialBalanceFragment.java
@@ -1,19 +1,28 @@
 package com.ecxfoi.wbl.wienerbergerfrontend.ui.main.materialbalance;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.ecxfoi.wbl.wienerbergerfrontend.R;
 import com.ecxfoi.wbl.wienerbergerfrontend.base.BaseFragment;
 import com.ecxfoi.wbl.wienerbergerfrontend.databinding.FragmentMaterialBalanceBinding;
 import com.ecxfoi.wbl.wienerbergerfrontend.databinding.FragmentMyAccountBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.models.MaterialBalanceData;
+import com.ecxfoi.wbl.wienerbergerfrontend.repositories.MaterialBalanceRepository;
 import com.ecxfoi.wbl.wienerbergerfrontend.ui.main.home.HomeViewModel;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -38,6 +47,26 @@ public class MaterialBalanceFragment extends BaseFragment<MaterialBalanceViewMod
         binding = FragmentMaterialBalanceBinding.inflate(inflater, container, false);
         binding.setMaterialBalanceViewModel(viewModel);
 
+        final Button buttonBack = binding.btnBack;
+
+        final Activity allMaterialsActivity = getActivity();
+        if (allMaterialsActivity != null)
+        {
+            buttonBack.setOnClickListener(v -> Navigation.findNavController(allMaterialsActivity, R.id.nav_host_fragment).navigate(R.id.action_materialBalanceFragment_to_homeFragment));
+        }
+
+        viewModel.fetchMaterialBalanceData().observe(getViewLifecycleOwner(), this::setMaterialBalanceData);
+
         return binding.getRoot();
+    }
+
+    private void setMaterialBalanceData(final ArrayList<MaterialBalanceData> materialBalanceData)
+    {
+        binding.listView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MaterialBalanceRecyclerAdapter recyclerAdapter = new MaterialBalanceRecyclerAdapter(materialBalanceData)
+        {
+
+        };
+        binding.listView.setAdapter(recyclerAdapter);
     }
 }
