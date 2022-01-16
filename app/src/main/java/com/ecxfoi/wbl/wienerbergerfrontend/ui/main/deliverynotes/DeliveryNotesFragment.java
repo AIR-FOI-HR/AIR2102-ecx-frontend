e8b0a3b77ccc0af537a7b23c0f3efd8bc3905aaa
Lovro <lovro.posaric@gmail.com>
2022-01-16 21:39:16 +0100

    WBL-69 Added Delivery notes interface elements

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/deliverynotes/DeliveryNotesFragment.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/deliverynotes/DeliveryNotesFragment.java
@@ -1,8 +1,26 @@
 package com.ecxfoi.wbl.wienerbergerfrontend.ui.main.deliverynotes;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.ecxfoi.wbl.wienerbergerfrontend.base.BaseFragment;
+import com.ecxfoi.wbl.wienerbergerfrontend.databinding.FragmentDeliveryNotesBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.databinding.FragmentMyAccountBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.ui.main.MainActivity;
+import com.ecxfoi.wbl.wienerbergerfrontend.ui.main.myaccount.MyAccountViewModel;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import javax.inject.Inject;
 
@@ -12,10 +30,70 @@ public class DeliveryNotesFragment extends BaseFragment<DeliveryNotesViewModel>
     ViewModelProvider.Factory factory;
 
     private DeliveryNotesViewModel viewModel;
+    private FragmentDeliveryNotesBinding binding;
+
+    private EditText txtDateFrom;
+    private EditText txtDateTo;
+
+    private EditTextDatePicker datePickerFrom;
+    private EditTextDatePicker datePickerTo;
 
     @Override
     public DeliveryNotesViewModel getViewModel() {
         viewModel = new ViewModelProvider(this, factory).get(DeliveryNotesViewModel.class);
         return viewModel;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        binding = FragmentDeliveryNotesBinding.inflate(inflater, container, false);
+        binding.setDeliveryNotesViewModel(viewModel);
+
+        txtDateFrom = binding.txtDeliveriesFrom;
+        txtDateTo = binding.txtDeliveriesTo;
+
+        datePickerFrom = new EditTextDatePicker(getActivity(), txtDateFrom);
+        datePickerTo = new EditTextDatePicker(getActivity(), txtDateTo);
+
+        return binding.getRoot();
+    }
+}
+
+class EditTextDatePicker implements View.OnClickListener, DatePickerDialog.OnDateSetListener
+{
+    Context context;
+    EditText editText;
+    private int day;
+    private int month;
+    private int birthYear;
+
+    public EditTextDatePicker(Context context, EditText editText)
+    {
+        this.context = context;
+        this.editText = editText;
+        this.editText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.birthYear = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+        updateDisplay();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+        DatePickerDialog dialog = new DatePickerDialog(context, this,
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    private void updateDisplay() {
+        editText.setText(new StringBuilder().append(day).append("/").append(month + 1).append("/").append(birthYear).append(" "));
     }
 }
---
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/main/deliverynotes/DeliveryNotesRecyclerAdapter.java
@@ -0,0 +1,76 @@
+package com.ecxfoi.wbl.wienerbergerfrontend.ui.main.deliverynotes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ecxfoi.wbl.wienerbergerfrontend.R;
+import com.ecxfoi.wbl.wienerbergerfrontend.models.DeliveryNoteData;
+
+import java.util.ArrayList;
+
+public class DeliveryNotesRecyclerAdapter extends RecyclerView.Adapter<DeliveryNotesRecyclerAdapter.DeliveryNotesViewHolder>
+{
+    private final ArrayList<DeliveryNoteData> deliveryNoteArray;
+
+    public static class DeliveryNotesViewHolder extends RecyclerView.ViewHolder
+    {
+        private final TextView tvID;
+        private final TextView tvName;
+        private final TextView tvShipToParty;
+        private final TextView tvDeliveredDate;
+
+        private DeliveryNoteData deliveryNoteData;
+
+        public DeliveryNotesViewHolder(final View view)
+        {
+            super(view);
+
+            tvID = view.findViewById(R.id.tv_delivery_note_id);
+            tvName = view.findViewById(R.id.tv_delivery_order_number);
+            tvShipToParty = view.findViewById(R.id.tv_delivery_ship_to_party);
+            tvDeliveredDate = view.findViewById(R.id.tv_delivery_delivered_date);
+        }
+
+        public void bind(DeliveryNoteData deliveryNoteData)
+        {
+            this.deliveryNoteData = deliveryNoteData;
+
+            tvID.setText(deliveryNoteData.getId().toString());
+            tvName.setText(deliveryNoteData.getOrderId().toString());
+            tvShipToParty.setText(deliveryNoteData.getDeliveryAddress());
+            tvDeliveredDate.setText(deliveryNoteData.getDeliveredDate());
+        }
+    }
+
+    public DeliveryNotesRecyclerAdapter(ArrayList<DeliveryNoteData> deliveryNoteArray)
+    {
+        this.deliveryNoteArray = deliveryNoteArray;
+    }
+
+    @NonNull
+    @Override
+    public DeliveryNotesViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+    {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.delivery_note_row_item, viewGroup, false);
+
+        return new DeliveryNotesRecyclerAdapter.DeliveryNotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DeliveryNotesViewHolder deliveryNotesViewHolder, final int position)
+    {
+        deliveryNotesViewHolder.bind(deliveryNoteArray.get(deliveryNoteArray.size() - 1 - position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return deliveryNoteArray.size();
+    }
+}
