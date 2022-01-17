18b9bcf361dabca9b3c054dbe736f3aac77569d5
Mislav Matijević <mislav.mim@gmail.com>
2022-01-17 21:11:53 +0100

    WBL-75 Modular fragment working Logging over modular fragment ClassicLoginFragment now works.

--- app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/login/LoginActivity.java
+++ app/src/main/java/com/ecxfoi/wbl/wienerbergerfrontend/ui/login/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.ecxfoi.wbl.wienerbergerfrontend.ui.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,25 +8,36 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import com.ecxfoi.wbl.classic_login.ui.ClassicLoginFragment;
+import com.ecxfoi.wbl.interface_login.LoginFragment;
 import com.ecxfoi.wbl.wienerbergerfrontend.api.JwtAuthInterceptor;
+import com.ecxfoi.wbl.wienerbergerfrontend.auth.AuthenticationData;
+import com.ecxfoi.wbl.wienerbergerfrontend.auth.AuthenticationInterface;
 import com.ecxfoi.wbl.wienerbergerfrontend.base.BaseActivity;
-import com.ecxfoi.wbl.wienerbergerfrontend.databinding.ActivityMainBinding;
+import com.ecxfoi.wbl.wienerbergerfrontend.models.WienerbergerResponse;
 import com.ecxfoi.wbl.wienerbergerfrontend.ui.companyselection.CompanySelectionActivity;
 import com.ecxfoi.wbl.wienerbergerfrontend.R;
 import com.ecxfoi.wbl.wienerbergerfrontend.auth.AuthService;
 import com.ecxfoi.wbl.wienerbergerfrontend.databinding.ActivityLoginBinding;
 
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import javax.inject.Inject;
+
+import retrofit2.Response;
 
 public class LoginActivity extends BaseActivity<LoginViewModel>
 {
     private ActivityLoginBinding binding;
 
     private NavController navController;
+
+    private LoginFragment destFragment;
 
     @Inject
     LoginViewModel viewModel;
@@ -49,27 +61,91 @@ public class LoginActivity extends BaseActivity<LoginViewModel>
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        AuthService.authenticationInterface = new AuthenticationInterface()
+        {
+            @Override
+            public void interpretResponse(final String email, final String password, final Response<WienerbergerResponse<AuthenticationData>> response)
+            {
+                WienerbergerResponse<AuthenticationData> wienerbergerResponse;
+                if (response.isSuccessful())
+                {
+                    wienerbergerResponse = response.body();
+                    AuthenticationData responseData = wienerbergerResponse.getData();
+                    String jwt = responseData.jwt;
+
+                    rememberUser(email, password);
+
+                    setResult(Activity.RESULT_OK);
+
+                    finish();
+                    switchToCompanySelection();
+
+                    authInterceptor.setJwtToken(jwt);
+                }
+                else
+                {
+                    int errorResCode = R.string.login_failed_generic;
+
+                    try
+                    {
+                        // Casting to real objects just didn't work so easier variant was implemented here:
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+
+                        errorResCode = R.string.login_failed_credentials;
+                    }
+                    catch (JSONException | IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                    destFragment.setErrorMessage(getResources().getString(errorResCode));
+                }
+            }
+
+            @Override
+            public void interpretError()
+            {
+                destFragment.setErrorMessage(getResources().getString(R.string.error_no_connection));
+            }
+        };
+
         navigateTo("classic");
     }
 
     private void navigateTo(String destination)
     {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment destFragment = new Fragment();
 
-        switch(destination)
+        switch (destination)
         {
             case "classic":
                 destFragment = ClassicLoginFragment.newInstance();
+
+                destFragment.<ClassicLoginFragment.LoginListener>setListener((String email, String password) -> {
+                    try
+                    {
+                        if (!StringUtils.contains(email, '@'))
+                        {
+                            email += "@wb.com";
+                        }
+
+                        AuthService.createLoginRequest(email, password);
+                    }
+                    catch (Exception e)
+                    {
+                        destFragment.setErrorMessage(getResources().getString(R.string.error_no_connection));
+                    }
+                });
                 break;
             case "pin":
                 break;
             case "fingerprint":
                 break;
-            default: break;
+            default:
+                break;
         }
 
-        ft.add(R.id.nav_host_fragment_login, destFragment);
+        ft.add(R.id.nav_host_fragment_login, (Fragment) destFragment);
         ft.commit();
     }
 
--- classic_login/src/main/java/com/ecxfoi/wbl/classic_login/ui/ClassicLoginFragment.java
+++ classic_login/src/main/java/com/ecxfoi/wbl/classic_login/ui/ClassicLoginFragment.java
@@ -8,17 +8,46 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.method.HideReturnsTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.ecxfoi.wbl.classic_login.R;
 import com.ecxfoi.wbl.classic_login.databinding.ClassicLoginFragmentBinding;
+import com.ecxfoi.wbl.interface_login.LoginFragment;
 
-public class ClassicLoginFragment extends Fragment
+public class ClassicLoginFragment extends Fragment implements LoginFragment
 {
     private ClassicLoginFragmentBinding binding;
     private ClassicLoginViewModel mViewModel;
+
+    @Override
+    public void setErrorMessage(final String errorMessage)
+    {
+        isInputAvaliable(true);
+        binding.errorMessage.setText(errorMessage);
+    }
+
+    public interface LoginListener
+    {
+        void onLoginAttempt(String email, String password);
+    }
+
+    private LoginListener mListener;
+
+    @Override
+    public <T> void setListener(final T listener)
+    {
+        mListener = (ClassicLoginFragment.LoginListener) listener;
+    }
+
+    private void isInputAvaliable(boolean enabled)
+    {
+        binding.email.setEnabled(enabled);
+        binding.password.setEnabled(enabled);
+        binding.passwordSwitch.setEnabled(enabled);
+    }
 
     public static ClassicLoginFragment newInstance()
     {
@@ -30,9 +59,31 @@ public class ClassicLoginFragment extends Fragment
     {
         binding = ClassicLoginFragmentBinding.inflate(inflater, container, false);
 
+        initNavigation();
+
         mViewModel = new ViewModelProvider(this).get(ClassicLoginViewModel.class);
-        // TODO: Use the ViewModel
 
         return binding.getRoot();
+    }
+
+    private void initNavigation()
+    {
+        binding.passwordSwitch.setOnCheckedChangeListener((compoundButton, isChecked) -> binding.password.setTransformationMethod(isChecked ? HideReturnsTransformationMethod.getInstance() : HideReturnsTransformationMethod.getInstance()));
+        binding.login.setOnClickListener(v -> {
+            if (binding.email.getText().toString().equals(""))
+            {
+                Toast.makeText(getContext(), "Please enter your email.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if (binding.password.getText().toString().equals(""))
+            {
+                Toast.makeText(getContext(), "Please enter your password.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            binding.errorMessage.setText("");
+            isInputAvaliable(false);
+            mListener.onLoginAttempt(binding.email.getText().toString(), binding.password.getText().toString());
+        });
     }
 }
